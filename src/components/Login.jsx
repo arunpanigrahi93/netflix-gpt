b324543f9c08b9cb4760ab6a2564c060fd41f6fa
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-07-09 13:25:55 +0530

    singin/signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg"
           alt="background-img"
         />
       </div>
+      <form className="w-4/12 absolute px-16 py-12 bg-black my-36 mx-auto right-0 left-0 rounded-lg text-white opacity-80">
+        <h1 className="font-bold text-3xl py-2 ">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-4 w-full bg-gray-700 rounded-sm"
+          />
+        )}
+
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-4 w-full bg-gray-700 rounded-sm"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-700 rounded-sm"
+        />
+        <button className="p-2 my-4 w-full bg-red-500 rounded-md">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm ? (
+            <>
+              New to Netflix? <span className="font-bold">Sign Up Now</span>
+            </>
+          ) : (
+            <>
+              Already registered? <span className="font-bold">Sign In Now</span>
+            </>
+          )}
+        </p>
+      </form>
     </div>
   );
 };
